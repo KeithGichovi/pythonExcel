38649b06575f7b69708316137d35f72a13a1ea1a
Keith Gichovi <kgichovi04@gmail.com>
2022-03-26 22:23:01 +0000

    current version can print everything as list of tuples

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from openpyxl import load_workbook
 
 ExcelFile = load_workbook("sampleData.xlsx")
+sheet = ExcelFile.active
+rows = sheet.rows
 
-print(ExcelFile.sheetnames)
+headers = [cell.value for cell in next(rows)]
+##print(headers)
+
+for row in rows:
+    data = {}
+    for title, cell in zip(headers, row):
+        data[title] = cell.value
+
+    print(data)
